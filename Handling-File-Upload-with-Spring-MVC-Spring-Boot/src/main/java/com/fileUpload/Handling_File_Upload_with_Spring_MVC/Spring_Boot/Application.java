7e6641671e182b82cf05b127870cfc342c58d01e
Raajneel-Ray <96514499+Raajneel-Ray@users.noreply.github.com>
2026-03-29 23:41:09 +0530

    upload in spring boot

---
+++ Handling-File-Upload-with-Spring-MVC-Spring-Boot/src/main/java/com/fileUpload/Handling_File_Upload_with_Spring_MVC/Spring_Boot/Application.java
@@ -0,0 +1,13 @@
+package com.fileUpload.Handling_File_Upload_with_Spring_MVC.Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
